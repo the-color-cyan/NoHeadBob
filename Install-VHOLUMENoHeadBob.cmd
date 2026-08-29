38f0757610c0e21a9ca0bea14068e05a9c20f091
cyan <bdwllmsn@gmail.com>
2026-08-29 15:47:37 -0500

    powershell scripts moved to scripts dir

--- Install-VHOLUMENoHeadBob.cmd
+++ Install-VHOLUMENoHeadBob.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Install-VHOLUMENoHeadBob.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0/scripts/Install-VHOLUMENoHeadBob.ps1"
 set "EXIT_CODE=%ERRORLEVEL%"
 if not "%EXIT_CODE%"=="0" (
     echo.
--- Uninstall-VHOLUMENoHeadBob.cmd
+++ Uninstall-VHOLUMENoHeadBob.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Uninstall-VHOLUMENoHeadBob.ps1" %*
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0/scripts/Uninstall-VHOLUMENoHeadBob.ps1" %*
 set "EXIT_CODE=%ERRORLEVEL%"
 if not "%EXIT_CODE%"=="0" (
     echo.
